13faeb1334f24390034e51eb7b224cc40f4d9f97
Diana <dianacarolina2ne1@gmail.com>
2026-06-03 21:27:14 -0500

    Creación de React

---
+++ Parqueadero/front-end/DAF_react/parqueadero/src/App.js
@@ -0,0 +1,7 @@
+import './App.css';
+//Muestra que pagina se mostrara primero
+import Inicio from './paginas/inicio';
+function App(){
+  return <Inicio/>;
+}
+export default App;
---
+++ Parqueadero/front-end/DAF_react/parqueadero/src/paginas/inicio.jsx
@@ -0,0 +1,8 @@
+function Inicio(){
+    return(
+        <div>
+            <h1>Bienvenido al parqueadero</h1>
+        </div>
+    )
+}
+export default Inicio;
